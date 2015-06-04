45c2da92708005b9b0f8d2ba6cc675ff6d2ff7ce
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-06-04 21:38:33 +0200

    CPS evaluator

---
+++ Haskell/CBNEval3CPS.hs
@@ -0,0 +1,20 @@
+{- Do CPS transformation, after CBNEval2BClosureConvCompl.
+ -}
+module CBNEval3CPS where
+import DeBrujin
+
+type Env = [DenVal]
+data ExpVal = Funct Term Env
+data DenVal = Thunk Term Env
+
+eval :: Term -> Env -> (ExpVal -> omega) -> omega
+eval (Var n)     e k =
+  let Thunk t e' = (e !! n)
+  in eval t e' k
+eval (App t0 t1) e k =
+  eval t0 e $ \(Funct t e') ->
+    eval t (Thunk t1 e : e') k
+eval (Abs t)     e k =
+  k (Funct t e)
+
+main t = eval t [] id
